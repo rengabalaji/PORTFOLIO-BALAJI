0099d98fe15b1e2c426e2501927dc8a7a10aa48c
RENGA BALAJI <rengabalaji07@gmail.com>
2025-12-12 15:18:27 +0000

    remove that bottom line

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,7 @@
 
 const Footer = () => {
   return (
-    <footer className="border-t border-border mt-24 md:mt-32 py-8">
+    <footer className="mt-24 md:mt-32 py-8">
       <div className="container mx-auto px-4 md:px-8 flex flex-col md:flex-row justify-between items-center gap-6">
         
       </div>
