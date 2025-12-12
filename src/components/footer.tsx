6e35d2885b22bf0cd9111a11f491d04b4198ca39
RENGA BALAJI <rengabalaji07@gmail.com>
2025-12-12 15:17:34 +0000

    remove © 2025 MotionVerse. All Rights Reserved.

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,9 +3,7 @@ const Footer = () => {
   return (
     <footer className="border-t border-border mt-24 md:mt-32 py-8">
       <div className="container mx-auto px-4 md:px-8 flex flex-col md:flex-row justify-between items-center gap-6">
-        <p className="text-sm text-muted-foreground">
-          © {new Date().getFullYear()} MotionVerse. All Rights Reserved.
-        </p>
+        
       </div>
     </footer>
   );
