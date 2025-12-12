df602dd6eb6380438625b5dcde549c30d0f820a5
RENGA BALAJI <rengabalaji07@gmail.com>
2025-12-12 15:08:04 +0000

    remove twitter and youtube option

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -76,8 +76,6 @@ export const codingProfiles = [
 export const socialLinks = [
   { name: 'LinkedIn', link: 'https://www.linkedin.com/in/renga-balaji-s-73251b328?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BGf%2B10adKTFSR%2FVe3GxsJYA%3D%3D', icon: Linkedin },
   { name: 'GitHub', link: 'https://github.com/rengabalaji', icon: Github },
-  { name: 'Twitter', link: '#', icon: Twitter },
-  { name: 'YouTube', link: '#', icon: Youtube },
   { name: 'Instagram', link: '#', icon: Instagram },
 ];
 
